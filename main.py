e2d38022e4cecc5294d56be28742188bb55d71ff
Siphiwe <stapisi2061@protonmail.com>
2022-10-19 21:14:52 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import whisper
+import streamlit as st
+import time
+import os
+from pathlib import Path
+
+path = Path("audio")
+model = whisper.load_model("base")
+
+
+def transcriber(audio_path):
+    audio = whisper.load_audio(audio_path)
+    result = model.transcribe(audio)
+    results = result["segments"]
+    return results
+
+
+def convert(seconds):
+    return time.strftime("%H:%M:%S", time.gmtime(seconds))
+
+
+def format_transcript(results):
+    final_transcript = []
+    for output in results:
+        timestamp = f'{convert(output["start"])}'
+        transcribed = output["text"]
+        section = f"[{timestamp[3:]}]{transcribed}\n"
+        final_transcript.append(section)
+    return final_transcript
+
+
+def list_to_plain_text(list_entry):
+    plain_text = ""
+    for i in list_entry:
+        plain_text += str(i)
+    return plain_text
+
+
+@st.cache
+def write_to_file(text, filename):
+    with open(filename, 'w') as f:
+        document = f.write(text)
+    return document
+
+
+st.header("Transcription made easier.")
+uploaded_file = st.file_uploader(label="Upload the audio-file you would like to transcribe.",
+                                 type=['mp3', 'm4a', 'wav'])
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    with open(os.path.join("audio", uploaded_file.name), "wb") as file:
+        file.write(uploaded_file.getbuffer())
+    with st.spinner("Working on the transcription..."):
+        transcript = transcriber(file.name)
+        formatted_transcript = format_transcript(transcript)
+        transcript_result_to_display = list_to_plain_text(formatted_transcript)
+        final_text = st.text_area(label="Your Transcript:", value=transcript_result_to_display, height=400)
+        st.download_button(label="Download Text as txt",
+                           data=final_text,
+                           file_name="Transcript.docx")
+    if bytes_data:
+        st.audio(bytes_data, format='audio/wav')
+        st.success("Done!")
