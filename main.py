c92b6128590f5d4f1056a1f2f07f0b9590fcf5f2
Siphiwe <stapisi2061@protonmail.com>
2022-10-24 21:02:02 +0200

    Adding statefulness to the app. Still need to refactor code and smooth it out.

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import streamlit as st
 import time
 import os
 from pathlib import Path
+import random
 
 path = Path("audio")
+# path_two = Path("transcripts")
 model = whisper.load_model("base")
 
 
@@ -36,15 +38,15 @@ def list_to_plain_text(list_entry):
     return plain_text
 
 
-@st.cache
-def write_to_file(text, filename):
-    with open(filename, 'w') as f:
-        document = f.write(text)
-    return document
+def write_to_file(text):
+    with open(os.path.join("transcripts", "Transcript.txt"), 'w') as f:
+        f.write(text)
+        delete_files("audio")
+    text_area()
 
 
 def delete_files(my_path):
-    for file in os.listdir(path):
+    for file in os.listdir(my_path):
         file_path = os.path.join(path, file)
         try:
             if os.path.isfile(file_path):
@@ -53,29 +55,58 @@ def delete_files(my_path):
             print(e)
 
 
+
+def text_area():
+    # st.text_area(label="Your Transcript:", value=text, height=400)
+    with open("transcripts/Transcript.txt") as file:
+        extract = file.read()
+    text = st.text_area(label="Your Transcript:", value=extract, height=400)
+    with open(os.path.join("transcripts", "Transcript.txt"), 'w') as f:
+        f.write(text)
+
+    bytes_data = st.session_state.new_audio.getvalue()
+
+    if bytes_data:
+        st.audio(st.session_state.new_audio.getvalue(), format='audio/wav')
+
+
+def transcribe_and_prepare_audio(new_file):
+    st.session_state["new_audio"] = new_file
+    if st.session_state.new_audio is not None:
+        with open(os.path.join("audio", st.session_state.new_audio.name), "wb") as file:
+            file.write(st.session_state.new_audio.getbuffer())
+
+            if st.session_state.first_run is None:
+                st.info('Working on you transcription. This may take a few minutes...', icon="ℹ")
+                transcript = transcriber(file.name)
+                formatted_transcript = format_transcript(transcript)
+                transcript_result_to_display = list_to_plain_text(formatted_transcript)
+                write_to_file(transcript_result_to_display)
+                st.session_state.first_run = 1
+
+
+if "new_audio" not in st.session_state:
+    st.session_state["new_audio"] = None
+
+if "current_text" not in st.session_state:
+    st.session_state["current_text"] = None
+
+if "first_run" not in st.session_state:
+    st.session_state["first_run"] = None
+
+st.set_page_config(
+    page_title="Personal Transcriber.",
+    layout="centered",
+    menu_items={
+        "About": "# This is a test."
+    }
+)
+
 st.header("Transcription made easier.")
 uploaded_file = st.file_uploader(label="Upload the audio-file you would like to transcribe.",
                                  type=['mp3', 'm4a', 'wav'])
 
-
-def transcribe_and_prepare_audio(new_file):
-    if new_file is not None:
-        bytes_data = new_file.getvalue()
-        with open(os.path.join("audio", new_file.name), "wb") as file:
-            file.write(uploaded_file.getbuffer())
-        with st.spinner("Working on the transcription..."):
-
-            transcript = transcriber(file.name)
-            formatted_transcript = format_transcript(transcript)
-            transcript_result_to_display = list_to_plain_text(formatted_transcript)
-            final_text = st.text_area(label="Your Transcript:", value=transcript_result_to_display, height=400)
-
-            st.download_button(label="Download as .txt file.",
-                               data=final_text,
-                               file_name="transcript.txt", on_click=delete_files("audio"))
-            if new_file:
-                st.audio(bytes_data, format='audio/wav')
-                st.success("Done!")
-
-
-transcribe_and_prepare_audio(uploaded_file)
+if st.session_state.first_run is None:
+    transcribe_and_prepare_audio(uploaded_file)
+else:
+    text_area()
