1dd5ea480894db6a0549c846facb4657b29eb2b2
Siphiwe <stapisi2061@protonmail.com>
2022-10-25 22:09:38 +0200

    Adding tabs for easier use. The app is a little faster. Still need to add smoother closing process for downloading transcript and starting a new transcript.

--- main.py
+++ main.py
@@ -41,17 +41,15 @@ def write_to_file(text):
     with open(os.path.join("transcripts", "Transcript.txt"), 'w') as f:
         f.write(text)
         delete_files("audio")
-    text_area()
+    st.session_state.current_text = text
+    text_area(st.session_state.current_text)
 
 
 def update_file():
     print(f"Entering the update function. 1")
-    print(st.session_state.current_text)
-    time.sleep(4)
     if st.session_state.current_text is not None:
         with open(os.path.join("transcripts", "Transcript.txt"), 'w') as f:
             f.write(st.session_state.current_text)
-            time.sleep(2)
             print(f"Leaving the update function.2")
     else:
         pass
@@ -67,75 +65,82 @@ def delete_files(my_path):
             print(e)
 
 
-def text_area():
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.download_button(
-            label="Download As .txt file.",
-            data=st.session_state.current_text,
-            file_name="YourTranscript.txt",
-            on_click=update_file()
-        )
-    with col2:
-        st.download_button(
-            label="Download As .docx file.",
-            data=st.session_state.current_text,
-            file_name="YourTranscript.docx",
-            on_click=update_file()
-        )
+def text_area(text):
+    with tab2:
+        print("Entering text area. 3")
+        running_text = st.text_area(label="Your Transcript:",
+                                    value=text,
+                                    height=500)
 
-    with col3:
-        if st.button(label="Start A new Transcription."):
-            delete_files("audio")
-            delete_files("transcripts")
-            for key in st.session_state.keys():
-                del st.session_state[key]
+        col1, col2, col3 = st.columns(3, gap="small")
+        with col1:
+            st.download_button(
+                label="Download As .txt file.",
+                data=running_text,
+                file_name="YourTranscript.txt",
+                on_click=update_file()
+            )
+        with col2:
+            st.download_button(
+                label="Download As .docx file.",
+                data=running_text,
+                file_name="YourTranscript.docx",
+                on_click=update_file()
+            )
 
-    bytes_data = st.session_state.new_audio.getvalue()
+        with col3:
+            if st.button(label="Start A new Transcription."):
+                delete_files("audio")
+                delete_files("transcripts")
+                for key in st.session_state.keys():
+                    del st.session_state[key]
 
-    if bytes_data:
-        st.audio(st.session_state.new_audio.getvalue(), format='audio/wav')
-        print("Leaving text area. 4")
-        print(st.session_state.current_text)
+        bytes_data = st.session_state.new_audio.getvalue()
 
-    print("Entering text area. 3")
-    with open("transcripts/Transcript.txt") as file:
-        extract = file.read()
-    st.session_state.current_text = st.text_area(label="Your Transcript:",
-                                                 value=extract,
-                                                 height=400)
+        if bytes_data:
+            st.audio(st.session_state.new_audio.getvalue(), format='audio/wav')
+            print("Leaving text area. 4")
+            print(running_text)
 
 
 def transcribe_and_prepare_audio(new_file):
-    st.session_state["new_audio"] = new_file
-    if st.session_state.new_audio is not None:
-        with open(os.path.join("audio", st.session_state.new_audio.name), "wb") as file:
-            file.write(st.session_state.new_audio.getbuffer())
+    with tab2:
+        st.session_state["new_audio"] = new_file
+        if st.session_state.new_audio is not None:
+            with open(os.path.join("audio", st.session_state.new_audio.name), "wb") as file:
+                file.write(st.session_state.new_audio.getbuffer())
 
-            if st.session_state.first_run is None:
-                st.info('Working on you transcription. This may take a few minutes...', icon="ℹ")
-                transcript = transcriber(file.name)
-                formatted_transcript = format_transcript(transcript)
-                transcript_result_to_display = list_to_plain_text(formatted_transcript)
-                write_to_file(transcript_result_to_display)
-                st.session_state.first_run = 1
+                if st.session_state.first_run is None:
+                    st.info('Working on you transcription. This may take a few minutes...', icon="ℹ")
+                    transcript = transcriber(file.name)
+                    formatted_transcript = format_transcript(transcript)
+                    transcript_result_to_display = list_to_plain_text(formatted_transcript)
+                    write_to_file(transcript_result_to_display)
+                    st.session_state.first_run = 1
 
 
-if "first_run" not in st.session_state:
-    st.session_state["first_run"] = None
-
+st.set_page_config(
+    page_title="My Personal Transcriber.",
+    layout="wide"
+)
 st.header("Transcription made easier.")
-st.session_state.audio = st.file_uploader(label="Upload the audio-file you would like to transcribe.",
-                                          type=['mp3', 'm4a', 'wav'])
+tab1, tab2 = st.tabs(["Upload", "Transcript Document"])
 
-if st.session_state.first_run is None:
-    print("Fresh Start")
-    if "new_audio" not in st.session_state:
-        st.session_state["new_audio"] = None
+with tab1:
+    if "first_run" not in st.session_state:
+        st.session_state["first_run"] = None
 
-    if "current_text" not in st.session_state:
-        st.session_state["current_text"] = None
+    uploaded_file = st.file_uploader(label="Upload the audio-file you would like to transcribe.",
+                                     type=['mp3', 'm4a', 'wav'])
 
-    transcribe_and_prepare_audio(st.session_state.audio)
-else:
-    text_area()
+    if st.session_state.first_run is None:
+        print("Fresh Start")
+        if "new_audio" not in st.session_state:
+            st.session_state["new_audio"] = None
+
+        if "current_text" not in st.session_state:
+            st.session_state["current_text"] = None
+
+        transcribe_and_prepare_audio(uploaded_file)
+    else:
+        text_area(st.session_state.current_text)
