ef38b18fa3fa65058c7f78491d62a4449e73257d
Siphiwe <stapisi2061@protonmail.com>
2022-10-25 17:48:44 +0200

    Smoothing out the text area changes to allow proper saving of transcript.Not yet perfect. Still needs cleaning out.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import whisper
 import streamlit as st
-import time
 import os
 from pathlib import Path
-import random
+import time
 
 path = Path("audio")
-# path_two = Path("transcripts")
+path_transcript = Path("transcripts")
 model = whisper.load_model("base")
 
 
@@ -45,6 +44,19 @@ def write_to_file(text):
     text_area()
 
 
+def update_file():
+    print(f"Entering the update function. 1")
+    print(st.session_state.current_text)
+    time.sleep(4)
+    if st.session_state.current_text is not None:
+        with open(os.path.join("transcripts", "Transcript.txt"), 'w') as f:
+            f.write(st.session_state.current_text)
+            time.sleep(2)
+            print(f"Leaving the update function.2")
+    else:
+        pass
+
+
 def delete_files(my_path):
     for file in os.listdir(my_path):
         file_path = os.path.join(path, file)
@@ -55,19 +67,43 @@ def delete_files(my_path):
             print(e)
 
 
-
 def text_area():
-    # st.text_area(label="Your Transcript:", value=text, height=400)
-    with open("transcripts/Transcript.txt") as file:
-        extract = file.read()
-    text = st.text_area(label="Your Transcript:", value=extract, height=400)
-    with open(os.path.join("transcripts", "Transcript.txt"), 'w') as f:
-        f.write(text)
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.download_button(
+            label="Download As .txt file.",
+            data=st.session_state.current_text,
+            file_name="YourTranscript.txt",
+            on_click=update_file()
+        )
+    with col2:
+        st.download_button(
+            label="Download As .docx file.",
+            data=st.session_state.current_text,
+            file_name="YourTranscript.docx",
+            on_click=update_file()
+        )
+
+    with col3:
+        if st.button(label="Start A new Transcription."):
+            delete_files("audio")
+            delete_files("transcripts")
+            for key in st.session_state.keys():
+                del st.session_state[key]
 
     bytes_data = st.session_state.new_audio.getvalue()
 
     if bytes_data:
         st.audio(st.session_state.new_audio.getvalue(), format='audio/wav')
+        print("Leaving text area. 4")
+        print(st.session_state.current_text)
+
+    print("Entering text area. 3")
+    with open("transcripts/Transcript.txt") as file:
+        extract = file.read()
+    st.session_state.current_text = st.text_area(label="Your Transcript:",
+                                                 value=extract,
+                                                 height=400)
 
 
 def transcribe_and_prepare_audio(new_file):
@@ -85,28 +121,21 @@ def transcribe_and_prepare_audio(new_file):
                 st.session_state.first_run = 1
 
 
-if "new_audio" not in st.session_state:
-    st.session_state["new_audio"] = None
-
-if "current_text" not in st.session_state:
-    st.session_state["current_text"] = None
-
 if "first_run" not in st.session_state:
     st.session_state["first_run"] = None
 
-st.set_page_config(
-    page_title="Personal Transcriber.",
-    layout="centered",
-    menu_items={
-        "About": "# This is a test."
-    }
-)
-
 st.header("Transcription made easier.")
-uploaded_file = st.file_uploader(label="Upload the audio-file you would like to transcribe.",
-                                 type=['mp3', 'm4a', 'wav'])
+st.session_state.audio = st.file_uploader(label="Upload the audio-file you would like to transcribe.",
+                                          type=['mp3', 'm4a', 'wav'])
 
 if st.session_state.first_run is None:
-    transcribe_and_prepare_audio(uploaded_file)
+    print("Fresh Start")
+    if "new_audio" not in st.session_state:
+        st.session_state["new_audio"] = None
+
+    if "current_text" not in st.session_state:
+        st.session_state["current_text"] = None
+
+    transcribe_and_prepare_audio(st.session_state.audio)
 else:
     text_area()
